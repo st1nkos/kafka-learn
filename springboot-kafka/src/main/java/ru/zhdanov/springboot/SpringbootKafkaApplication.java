453e94c8b16c9e12f4dd048fa204c103357c6063
Anton <42771166+st1nkos@users.noreply.github.com>
2023-05-20 13:12:29 +0500

    Add files via upload

---
+++ springboot-kafka/src/main/java/ru/zhdanov/springboot/SpringbootKafkaApplication.java
@@ -0,0 +1,13 @@
+package ru.zhdanov.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaApplication.class, args);
+	}
+
+}
